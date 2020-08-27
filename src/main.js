9863e82b806124a0722fd626b7565ff8eb3bd0d5
gadzorgenu2000 <42786156+gadzorgenu2000@users.noreply.github.com>
2020-08-27 11:55:24 +0000

    Meged files

--- src/main.js
+++ src/main.js
@@ -26,3 +26,29 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+// Display the first tab by default
+document.getElementById("defaultOpen").click();
+
+function openTab(evt, tabName) {
+  // Declare variables
+  var i, tabcontent, tablinks, tabFastFood;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tab_content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+  //tabFastFood = document.querySelector("#fast_food");
+  //tabFastFood.classList.remove("active");
+}
